5905031657ff13fb7996dd753ff2287503359a7b
Marie-P <47229106+Marie-P@users.noreply.github.com>
2019-03-18 10:57:02 +0100

    squelette

---
+++ horlogeWidget.js
@@ -0,0 +1,22 @@
+//class hologeWidget extends Widget
+	//constructor Model,View,Controller
+
+	//seUup
+		//header
+		//footer
+
+	//ready
+
+//class hologeModel extends WidgetModel
+
+	//constructor() super
+
+	// horloge()
+
+//class horlogeView extends WidgetView
+	//constructor() super
+
+	//draw()
+
+//class horlogeController extends WidgetController
+	//constructor() super
