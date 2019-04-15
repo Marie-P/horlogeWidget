094b29a1edd4fdfb7d80fb3bb57cf1c405320a9d
Marie-P <47229106+Marie-P@users.noreply.github.com>
2019-04-15 11:57:10 +0200

    Update horlogeWidget.js

--- js/horlogeWidget.js
+++ js/horlogeWidget.js
@@ -7,7 +7,7 @@ class horlogeWidget extends Widget {
 	setUp() {
 		super.setUp();
 		this.header = true;
-		this.footer = false;
+		this.footer = true;
 		this.sizeX = 2;
 		this.sizeY = 2;
 		this.radius = 15;
@@ -37,21 +37,51 @@ class horlogeView extends WidgetView {
 	draw() {
 		super.draw();
 
-		this.try.input = document.createElement("input");
-
-		
+		// On crée un canvas dans le widget
 		let toDraw = document.createElement("canvas");
+
+
 		toDraw.width = 150 * this.try.mvc.main.sizeX;
-		toDraw.height = 150 * this.try.mvc.main.sizeY - 25;
+		toDraw.height = 150 * this.try.mvc.main.sizeY - 50;
 		this.try.stage.appendChild(toDraw);
 
 		let clock = toDraw.getContext('2d');
 
+		function drawAClock()
+		{
+			// to get time
+			let time = new Date();
+			let hours = time.getHours(); 
+			let minutes = time.getMinutes();
+			let seconds = time.getSeconds();
+
+			// va effacer notre horloge à chaque fois pour la retracer
+			clock.clearRect(0, 0, toDraw.width, toDraw.height);
+
+			clock.beginPath(); 
+			clock.arc(toDraw.width/2, toDraw.height/2, toDraw.height/2, 0, 2 * Math.PI);
+
+			// Les chiffres de l'horloge
+			clock.font = toDraw.height/10 + 'px arial';
+
+			for(var i = 1; i <= 12; i++)
+			{
+				let numberClock = toVec(radians((360/12) * i - 90));
+				clock.fillText(i, numberClock[0] * toDraw.width/3 + toDraw.width/2.1, numberClock[1] * toDraw.height/2.5 + toDraw.height/1.9);
+			}
+
+			// Trace les aiguilles
+			needle(toVec(radians(changeMinutes(minutes))), toDraw.width/2.5);
+			needle(toVec(radians(changeHours(hours))), toDraw.width/5);
+			needle(toVec(radians(changeMinutes(seconds))), toDraw.width/2.5);
+
+			clock.stroke();
+		}
 
 		function changeHours(h)
 		{
-			return (360/12) * h - 90; // Calcul d'angle (360/12) à partir de l'heure récupérée
-
+			// Calcul d'angle (360/12) à partir de l'heure récupérée
+			return (360/12) * h - 90;
 		}
 
 		function changeMinutes(min)
@@ -86,30 +116,6 @@ class horlogeView extends WidgetView {
 			clock.lineTo(vec[0] + toDraw.width/2,vec[1] + toDraw.height/2);
 		}
 
-		function drawAClock()
-		{
-		
-			clock.clearRect(0, 0, toDraw.width, toDraw.height);
-			clock.beginPath(); 
-			clock.arc(toDraw.width/2, toDraw.height/2, toDraw.height/2, 0, 2 * Math.PI);
-
-			clock.stroke();
-			let time = new Date();
-			let hours = time.getHours(); 
-			let minutes = time.getMinutes();
-			let seconds = time.getSeconds();
-
-			clock.restore();
-			clock.beginPath(); 
-		
-
-			needle(toVec(radians(changeMinutes(minutes))), toDraw.width/2.5);
-			needle(toVec(radians(changeHours(hours))), toDraw.width/5);
-			needle(toVec(radians(changeMinutes(seconds))), toDraw.width/2.5);
-
-			clock.stroke();
-		}
-
 		function update()
 		{
 			var intervalID = window.setInterval(drawAClock, 1000);	
@@ -127,15 +133,6 @@ class horlogeController extends WidgetController {
 	constructor() {
 		super();
 	}
-
-	async refreshDate()
-	{
-		let ville = this.try.mvc.view.input.value;
-		let result = await this.mvc.main.dom("https://maps.googleapis.com/maps/api/geocode/json?address=San%20Francisco,%20CA&sensor=false&key=AIzaSyCsyqpek1aix-THp5VZSUDBhAf9AoaeT7M");
-		console.log(result);
-		/// faire requête à google maps avec la ville
-	}
-
 }
 
 
